3094c6ec9b9f5d7a15d64f894444a4f747f99043
kasun Chandimal <39762853+kasunChandi@users.noreply.github.com>
2020-07-19 02:58:33 -0700

    ios inatial commit
    ios working on programm

---
+++ iosAssignment/tableView.swift
@@ -0,0 +1,15 @@
+//
+//  tableView.swift
+//  iosAssignment
+//
+//  Created by STUDENT on 7/19/20.
+//  Copyright © 2020 STUDENT. All rights reserved.
+//
+
+import UIKit
+
+class tableView: NSObject {
+    
+    
+
+}
